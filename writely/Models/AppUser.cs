a161962fc93b9fce1b252e6a3b73bcc8cbdc4757
Kianek <kallridge92@gmail.com>
2020-08-14 13:36:34 -0700

    Add IsAccountActive

--- writely/Models/AppUser.cs
+++ writely/Models/AppUser.cs
@@ -9,6 +9,7 @@ namespace writely.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public bool IsAccountActive { get; set; }
 
         public AppUser()
         {
@@ -18,6 +19,7 @@ namespace writely.Models
         {
             FirstName = user.FirstName;
             LastName = user.LastName;
+            IsAccountActive = false;
             base.Email = user.Email;
             base.UserName = user.Username;
         }
