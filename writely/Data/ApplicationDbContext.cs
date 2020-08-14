fd397f481d3bc4ef11d345083d8479874888d970
Kianek <kallridge92@gmail.com>
2020-08-14 13:12:57 -0700

    Add model configurations

--- writely/Data/ApplicationDbContext.cs
+++ writely/Data/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using writely.Models;
 
 namespace writely.Data
 {
-    public class ApplicationDbContext : DbContext
-           {
-        public DbSet<AppUser> Users { get; set; }
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
         public DbSet<Journal> Journals { get; set; }
         public DbSet<Entry> Entries { get; set; }
 
@@ -16,7 +16,57 @@ namespace writely.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<AppUser>(u =>
+            {
+                u.Property(usr => usr.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(255);
+                
+                
+                u.Property(usr => usr.LastName)
+                    .IsRequired()
+                    .HasMaxLength(255);
+            });
 
+            builder.Entity<Journal>(j =>
+            {
+                j.Property(journal => journal.Title)
+                    .HasMaxLength(255)
+                    .IsRequired();
+
+                j.Property(journal => journal.Id)
+                    .IsRequired();
+
+                j.Property(journal => journal.UserId)
+                    .IsRequired();
+
+                j
+                    .HasMany(journal => journal.Entries)
+                    .WithOne(entry => entry.Journal);
+            });
+
+            builder.Entity<Entry>(entry =>
+            {
+                entry.Property(e => e.Title)
+                    .HasMaxLength(255)
+                    .IsRequired();
+
+                entry.Property(e => e.Body)
+                    .HasMaxLength(3000)
+                    .IsRequired();
+
+                entry.Property(e => e.UserId)
+                    .IsRequired();
+
+                entry.Property(e => e.Username)
+                    .IsRequired();
+
+                entry
+                    .HasOne(e => e.Journal)
+                    .WithMany(j => j.Entries);
+            });
         }
     }
 }
