9cc59b9a5e9a5570c5aa6cc3958e362e26fd2dde
Kianek <kallridge92@gmail.com>
2020-08-14 13:22:59 -0700

    Add name properties and constructor

--- writely/Models/AppUser.cs
+++ writely/Models/AppUser.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using Microsoft.AspNetCore.Identity;
 using writely.Data;
+using writely.Models.Dto;
 
 namespace writely.Models
 {
     public class AppUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
         public AppUser()
         {
+        }
+
+        public AppUser(UserRegistrationDto user)
+        {
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            base.Email = user.Email;
+            base.UserName = user.Username;
         }
     }
 }
