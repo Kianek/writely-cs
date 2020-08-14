ca936f4e1dca568aecd2288f96f7b5171302d2c0
Kianek <kallridge92@gmail.com>
2020-08-14 13:13:35 -0700

    Initial commit

---
+++ writely/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using writely.Models;
+using writely.Models.Dto;
+
+namespace writely.Services
+{
+    public class UserService : IUserService
+    {
+        private UserManager<AppUser> userManager;
+
+        public UserService(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public int Register(UserRegistrationDto registration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteAccount(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DisableAccount(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserData GetUserData(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
