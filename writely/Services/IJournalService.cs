f0913877ed39226c1ff7e61d9db0ccfd21a3f7af
Kianek <kallridge92@gmail.com>
2020-08-23 08:59:51 -0700

    Update return types

--- writely/Services/IJournalService.cs
+++ writely/Services/IJournalService.cs
@@ -7,10 +7,10 @@ namespace writely.Services
 {
     public interface IJournalService
     {
-        Task<Journal> Add(string userId, string title);
-        Task<Journal> Update(JournalDto updatedJournal);
+        Task<JournalDto> Add(string userId, string title);
+        Task<JournalDto> Update(JournalDto updatedJournal);
         Task Delete(long id);
-        Task<List<Journal>> GetAll(string userId, int limit = 0);
-        Task<Journal> GetById(long id);
+        Task<List<JournalDto>> GetAll(string userId, int limit = 0);
+        Task<JournalDto> GetById(long id);
     }
 }
