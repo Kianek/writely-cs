a61c615685fdb58c06bcbdd5a57ea361ed161c3d
Kianek <kallridge92@gmail.com>
2020-08-27 18:12:28 -0700

    Remove unused usings

--- writely/Controllers/JournalsController.cs
+++ writely/Controllers/JournalsController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using writely.Data;
 using writely.Models.Dto;
 using writely.Services;
 
