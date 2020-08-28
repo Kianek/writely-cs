0dfb1e938f13cc43fc1ef670d74179443a58b7b4
Kianek <kallridge92@gmail.com>
2020-08-28 10:51:33 -0700

    Remove unused usings

--- writely/Services/IEntryService.cs
+++ writely/Services/IEntryService.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using writely.Models;
 using writely.Models.Dto;
 
 namespace writely.Services
