e24c7b006825f1ece02e599ae8f36805f70d206f
Kianek <kallridge92@gmail.com>
2020-08-31 08:34:18 -0700

    Passing DeleteAccount test; do some refactoring

--- writely.integration-tests/Users/UsersControllerTest.cs
+++ writely.integration-tests/Users/UsersControllerTest.cs
@@ -1,20 +1,29 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using writely.Data;
 using writely.Models.Dto;
+using writely.Services;
 using Xunit;
 
 namespace writely.integration_tests
 {
-    public class UsersControllerTest : IClassFixture<WebAppFactory<writely.Startup>>
+    public class UsersControllerTest : IClassFixture<WebAppFactory<writely.Startup>>, IDisposable
     {
         private HttpClient _client;
+        private WebAppFactory<Startup> _factory;
+        private IServiceScope _scope;
+        private IServiceProvider _services;
 
         public UsersControllerTest(WebAppFactory<Startup> factory)
         {
             _client = factory.CreateClient();
             _client.BaseAddress = new Uri("http://localhost:5000");
+            _factory = factory;
+            _scope = factory.Services.CreateScope();
+            _services = _scope.ServiceProvider;
         }
 
         [Fact]
@@ -32,6 +41,34 @@ namespace writely.integration_tests
 
             using var response = await _client.PostAsync("/api/users", newUserJson.AsStringContent());
             response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task DeleteAccount()
+        {
+            var newUser = new UserRegistrationDto
+            {
+                Email = "john@doemail.com",
+                FirstName = "John",
+                LastName = "Doe",
+                Password = "Password123!",
+                ConfirmPassword = "Password123!",
+                Username = "j.doe"
+            };
+
+            var context = _services.GetRequiredService<ApplicationDbContext>();
+            var service = _services.GetRequiredService<IUserService>();
+            await service.Register(newUser);
+            var user = await context.Users.FirstOrDefaultAsync(u
+                => u.Email == newUser.Email);
+
+            var response = await _client.DeleteAsync($"api/users/{user?.Id}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
         }
     }
 }
