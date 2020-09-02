ff32354c981c5ae7f46d0409637c552d781c3cb5
Keanu Allridge <kallridge92@gmail.com>
2020-09-01 18:05:55 -0700

    Refactor infrastructural code into IntegrationTestBase class

--- writely.integration-tests/Users/UsersControllerTest.cs
+++ writely.integration-tests/Users/UsersControllerTest.cs
@@ -1,30 +1,12 @@
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using writely.Data;
-using writely.Models.Dto;
-using writely.Services;
 using Xunit;
 
 namespace writely.integration_tests.Users
 {
-    public class UsersControllerTest : IClassFixture<WebAppFactory<Startup>>, IDisposable
+    public class UsersControllerTest : IntegrationTestBase, IClassFixture<WebAppFactory<Startup>>
     {
-        private HttpClient _client;
-        private WebAppFactory<Startup> _factory;
-        private IServiceScope _scope;
-        private IServiceProvider _services;
-
-        public UsersControllerTest(WebAppFactory<Startup> factory)
-        {
-            _client = factory.CreateClient();
-            _client.BaseAddress = new Uri("http://localhost:5000");
-            _factory = factory;
-            _scope = factory.Services.CreateScope();
-            _services = _scope.ServiceProvider;
-        }
+        public UsersControllerTest(WebAppFactory<Startup> factory) : base(factory)
+        {}
 
         [Fact]
         public async Task RegisterUser()
@@ -38,14 +20,7 @@ namespace writely.integration_tests.Users
         [Fact]
         public async Task ActivateAccount()
         {
-            var newUser = Helpers.CreateRegistrationDto();
-
-            var context = _services.GetRequiredService<ApplicationDbContext>();
-            var service = _services.GetRequiredService<IUserService>();
-            await service.Register(newUser);
-            var user = await context.Users.FirstOrDefaultAsync(u => 
-                u.Email == newUser.Email);
-
+            var user = await SetUpUser();
             var response = await _client.PatchAsync($"/api/users/{user?.Id}/activate", null);
             response.EnsureSuccessStatusCode();
         }
@@ -53,14 +28,7 @@ namespace writely.integration_tests.Users
         [Fact]
         public async Task DeleteAccount()
         {
-            var newUser = Helpers.CreateRegistrationDto();
-
-            var context = _services.GetRequiredService<ApplicationDbContext>();
-            var service = _services.GetRequiredService<IUserService>();
-            await service.Register(newUser);
-            var user = await context.Users.FirstOrDefaultAsync(u
-                => u.Email == newUser.Email);
-
+            var user = await SetUpUser();
             var response = await _client.DeleteAsync($"api/users/{user?.Id}");
             response.EnsureSuccessStatusCode();
         }
@@ -68,20 +36,9 @@ namespace writely.integration_tests.Users
         [Fact]
         public async Task DisableAccount()
         {
-            var newUser = Helpers.CreateRegistrationDto();
-
-            var context = _services.GetRequiredService<ApplicationDbContext>();
-            var service = _services.GetRequiredService<IUserService>();
-            await service.Register(newUser);
-            var user = await context.Users.FirstOrDefaultAsync(u => u.Email == newUser.Email);
-
+            var user = await SetUpUser();
             var response = await _client.PatchAsync($"/api/users/{user?.Id}/disable", null);
             response.EnsureSuccessStatusCode();
-        }
-
-        public void Dispose()
-        {
-            _scope.Dispose();
         }
     }
 }
