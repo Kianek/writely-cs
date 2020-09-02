ec6a339b78ba95ab8322f12bd9d3d5ac7ddf5005
Kianek <kallridge92@gmail.com>
2020-09-02 16:36:16 -0700

    Passing GetOne test

--- writely.integration-tests/Journals/JournalsControllerTest.cs
+++ writely.integration-tests/Journals/JournalsControllerTest.cs
@@ -10,16 +10,24 @@ namespace writely.integration_tests.Journals
     {
         private AppUser _user;
         private IJournalService _service;
+        private string _baseUrl;
         
         public JournalsControllerTest(WebAppFactory<Startup> factory) : base(factory)
         {
             _user = SetUpUser().Result;
+            _baseUrl = $"/api/users/{_user.Id}/journals";
             _service = _services.GetRequiredService<IJournalService>();
         }
-        
+
         [Fact]
         public async Task GetOne()
-        {}
+        {
+            var journal = await _service.Add(_user.Id, "Super Duper Journal");
+            var url = $"{_baseUrl}/{journal.Id}";
+            
+            var response = await _client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+        }
         
         [Fact]
         public async Task GetAll()
