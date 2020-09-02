e4b67a2e9547a8300cdb936fc4fce5c2f1c2a377
Kianek <kallridge92@gmail.com>
2020-09-02 11:25:11 -0700

    Refactor tests

--- writely.integration-tests/Users/UsersControllerTest.cs
+++ writely.integration-tests/Users/UsersControllerTest.cs
@@ -1,44 +1,79 @@
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using writely.Models;
+using writely.Models.Dto;
 using Xunit;
 
 namespace writely.integration_tests.Users
 {
-    public class UsersControllerTest : IntegrationTestBase, IClassFixture<WebAppFactory<Startup>>
+    public class UsersControllerTest
     {
-        public UsersControllerTest(WebAppFactory<Startup> factory) : base(factory)
-        {}
-
-        [Fact]
-        public async Task RegisterUser()
+        public class UnregisteredUserTest : IntegrationTestBase, IClassFixture<WebAppFactory<Startup>>
         {
-            var newUserJson = Helpers.CreateRegistrationDto();
+            public UnregisteredUserTest(WebAppFactory<Startup> factory) : base(factory)
+            {
+            }
 
-            using var response = await _client.PostAsync("/api/users", newUserJson.AsStringContent());
-            response.EnsureSuccessStatusCode();
+            [Fact]
+            public async Task RegisterUser()
+            {
+                var newUserJson = Helpers.CreateRegistrationDto();
+
+                using var response = await _client.PostAsync("/api/users", newUserJson.AsStringContent());
+                response.EnsureSuccessStatusCode();
+            }
         }
 
-        [Fact]
-        public async Task ActivateAccount()
+
+        public class ChangePasswordTest : IntegrationTestBase, IClassFixture<WebAppFactory<Startup>>
         {
-            var user = await SetUpUser();
-            var response = await _client.PatchAsync($"/api/users/{user?.Id}/activate", null);
-            response.EnsureSuccessStatusCode();
+            public ChangePasswordTest(WebAppFactory<Startup> factory) : base(factory)
+            {
+            }
+
+            [Fact]
+            public async Task ChangePassword()
+            {
+                var user = await SetUpUser();
+                var passwordUpdateDto = new UpdatePasswordDto(
+                    "Password123!", "Skibbitydibbity123!");
+                var request = passwordUpdateDto.AsStringContent();
+                var response = await _client.PatchAsync($"/api/users/{user?.Id}/change-password", request);
+                response.EnsureSuccessStatusCode();
+            }
         }
 
-        [Fact]
-        public async Task DeleteAccount()
+        public class RegisteredUserTest : IntegrationTestBase, IClassFixture<WebAppFactory<Startup>>
         {
-            var user = await SetUpUser();
-            var response = await _client.DeleteAsync($"api/users/{user?.Id}");
-            response.EnsureSuccessStatusCode();
-        }
+            private readonly AppUser _user;
 
-        [Fact]
-        public async Task DisableAccount()
-        {
-            var user = await SetUpUser();
-            var response = await _client.PatchAsync($"/api/users/{user?.Id}/disable", null);
-            response.EnsureSuccessStatusCode();
+            public RegisteredUserTest(WebAppFactory<Startup> factory) : base(factory)
+            {
+                _user = SetUpUser().Result;
+                _scope = factory.Services.CreateScope();
+                _services = _scope.ServiceProvider;
+            }
+
+            [Fact]
+            public async Task ActivateAccount()
+            {
+                var response = await _client.PatchAsync($"/api/users/{_user?.Id}/activate", null);
+                response.EnsureSuccessStatusCode();
+            }
+
+            [Fact]
+            public async Task DisableAccount()
+            {
+                var response = await _client.PatchAsync($"/api/users/{_user?.Id}/disable", null);
+                response.EnsureSuccessStatusCode();
+            }
+
+            [Fact]
+            public async Task DeleteAccount()
+            {
+                var response = await _client.DeleteAsync($"/api/users/{_user?.Id}");
+                response.EnsureSuccessStatusCode();
+            }
         }
     }
 }
