c52700e963124aa005cd76dd0528f8870ede5ca8
Kianek <kallridge92@gmail.com>
2020-09-04 20:38:37 -0700

    Add authorization

--- writely/Controllers/JournalsController.cs
+++ writely/Controllers/JournalsController.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using writely.Models.Dto;
@@ -6,6 +7,7 @@ using writely.Services;
 
 namespace writely.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/users/{userId}/journals")]
     public class JournalsController : ControllerBase
