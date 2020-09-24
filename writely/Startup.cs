b31edd50a8f5a2b7ae81afc3c9819ebeecf62e0d
Kianek <kallridge92@gmail.com>
2020-09-24 08:47:41 -0700

    Fix CORS issue

--- writely/Startup.cs
+++ writely/Startup.cs
@@ -23,6 +23,8 @@ namespace writely
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+        
             services.AddControllers();
             
             services.AddTransient<IAuthService, AuthService>();
@@ -59,11 +61,19 @@ namespace writely
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors();
-
+            app.UseRouting();
+            
+            app.UseCors(x =>
+            {
+                x
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed(origin => true)
+                    .AllowCredentials();
+            });
+            
             app.UseHttpsRedirection();
 
-            app.UseRouting();
 
             app.UseAuthentication();
 
