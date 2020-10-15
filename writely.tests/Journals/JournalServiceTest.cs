d8f8dccc57351c64ade414879c5ed197c94026da
Kianek <kallridge92@gmail.com>
2020-10-15 12:12:23 -0700

    Some refactoring

--- writely.tests/Journals/JournalServiceTest.cs
+++ writely.tests/Journals/JournalServiceTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using FluentAssertions;
+using writely.Data;
 using writely.Models;
 using writely.Models.Dto;
 using writely.Services;
@@ -11,6 +12,7 @@ namespace writely.tests.Journals
     public class JournalServiceTest :  IDisposable
     {
         private readonly DatabaseFixture _fixture;
+        private ApplicationDbContext _context;
         
         public JournalServiceTest()
         {
@@ -23,14 +25,12 @@ namespace writely.tests.Journals
             const string userId = "UserId";
             const string title = "Super Nifty Journal";
 
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
+            await PrepareDatabase();
             
-            var service = new JournalService(context);
+            var service = new JournalService(_context);
             var result = await service.Add(userId, title);
     
-            var addedJournal = await context.Journals.FindAsync(result.Id);
+            var addedJournal = await _context.Journals.FindAsync(result.Id);
             addedJournal.Title.Should().Be(title);
         }
 
@@ -39,12 +39,13 @@ namespace writely.tests.Journals
         {
             const string userId = "UserId";
             const string title = "Super Samey Title";
+
+            await PrepareDatabase();
             
-            await using var context = _fixture.CreateContext();
-            context.Journals.Add(new Journal {UserId = userId, Title = title});
-            await context.SaveChangesAsync();
+            _context.Journals.Add(new Journal {UserId = userId, Title = title});
+            await _context.SaveChangesAsync();
             
-            var service = new JournalService(context);
+            var service = new JournalService(_context);
             var result = await service.Add(userId, title);
 
             result.Should().BeNull();
@@ -56,14 +57,13 @@ namespace writely.tests.Journals
             const string userId = "UserId";
             const string title = "Old Title";
             const string newTitle = "New Title";
+
+            await PrepareDatabase();
             
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            context.Journals.Add(new Journal {Id = 1L, UserId = userId, Title = title});
-            await context.SaveChangesAsync();
+            _context.Journals.Add(new Journal {Id = 1L, UserId = userId, Title = title});
+            await _context.SaveChangesAsync();
             
-            var service = new JournalService(context);
+            var service = new JournalService(_context);
             var updatedJournal = new JournalDto
             {
                 Title = newTitle,
@@ -71,7 +71,7 @@ namespace writely.tests.Journals
                 Id = 1L
             };
             var result = await service.Update(updatedJournal);
-            var updJournal = await context.Journals.FindAsync(updatedJournal.Id);
+            var updJournal = await _context.Journals.FindAsync(updatedJournal.Id);
 
             result.Title.Should().Be(updJournal.Title);
         }
@@ -79,10 +79,9 @@ namespace writely.tests.Journals
         [Fact]
         public async void Update_JournalNotFound_NotUpdated()
         {
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            var service = new JournalService(context);
+            await PrepareDatabase();
+            
+            var service = new JournalService(_context);
             var journal = new JournalDto {Id = 1L};
             
             var result = await service.Update(journal);
@@ -94,16 +93,15 @@ namespace writely.tests.Journals
         {
             var journal = new Journal { Id = 1L, Title = "Blah", UserId = "UserId"};
 
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            context.Journals.Add(journal);
-            await context.SaveChangesAsync();
+            await PrepareDatabase();
+
+            _context.Journals.Add(journal);
+            await _context.SaveChangesAsync();
             
-            var service = new JournalService(context);
+            var service = new JournalService(_context);
             await service.Delete(journal.Id);
             
-            var result = await context.Journals.FindAsync(journal.Id);
+            var result = await _context.Journals.FindAsync(journal.Id);
             result.Should().BeNull();
         }
         
@@ -112,17 +110,16 @@ namespace writely.tests.Journals
         {
             const string userId = "UserId";
 
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            context.Journals.AddRange(
+            await PrepareDatabase();
+
+            _context.Journals.AddRange(
                 new Journal {Title = "One", UserId = userId},
                 new Journal {Title = "Two", UserId = userId},
                 new Journal {Title = "Three", UserId = userId}
                 );
-            await context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
             
-            var service = new JournalService(context);
+            var service = new JournalService(_context);
             var result = await service.GetAll(userId);
 
             result.Count.Should().Be(3);
@@ -132,16 +129,13 @@ namespace writely.tests.Journals
         public async void GetAll_UserFound_JournalLimit_ReturnsLimitedJournals()
         {
             const string userId = "UserId";
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            for (var i = 0; i < 8; i++)
-            {
-                context.Journals.Add(new Journal {Title = $"Journal {i+1}", UserId = userId});
-            }
-            await context.SaveChangesAsync();
             
-            var service = new JournalService(context);
+            await PrepareDatabase();
+            
+            PopulateJournals(userId);
+            await _context.SaveChangesAsync();
+            
+            var service = new JournalService(_context);
             var result = await service.GetAll(userId, page: 3);
 
             result.Should().HaveCount(3);
@@ -151,16 +145,13 @@ namespace writely.tests.Journals
         public async void GetAll_UserFound_NoLimit_ReturnsAllJournals()
         {
             const string userId = "UserId";
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            for (var i = 0; i < 8; i++)
-            {
-                context.Journals.Add(new Journal {Title = $"Journal {i+1}", UserId = userId});
-            }
-            await context.SaveChangesAsync();
+
+            await PrepareDatabase();
             
-            var service = new JournalService(context);
+            PopulateJournals(userId);
+            await _context.SaveChangesAsync();
+            
+            var service = new JournalService(_context);
             var result = await service.GetAll(userId);
 
             result.Should().HaveCount(8);
@@ -171,22 +162,38 @@ namespace writely.tests.Journals
         {
             const long id = 1L;
             const string title = "Blah";
-            await using var context = _fixture.CreateContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-            context.Journals.Add(new Journal {Id = id, Title = title, UserId = "UserId"});
-            await context.SaveChangesAsync();
+
+            await PrepareDatabase();
             
-            var service = new JournalService(context);
+            _context.Journals.Add(new Journal {Id = id, Title = title, UserId = "UserId"});
+            await _context.SaveChangesAsync();
+            
+            var service = new JournalService(_context);
             var result = await service.GetById(id);
 
             result.Should().NotBeNull();
             result.Title.Should().Be(title);
         }
 
+        private void PopulateJournals(string userId)
+        {
+            for (var i = 0; i < 8; i++)
+            {
+                _context.Journals.Add(new Journal {Title = $"Journal {i+1}", UserId = userId});
+            }
+        }
+
+        private async Task PrepareDatabase()
+        {
+            _context = _fixture.CreateContext();
+            await _context.Database.EnsureDeletedAsync();
+            await _context.Database.EnsureCreatedAsync();
+        }
+
         public void Dispose()
         {
             _fixture.Dispose();
+            _context.Dispose();
         }
     }
 }
