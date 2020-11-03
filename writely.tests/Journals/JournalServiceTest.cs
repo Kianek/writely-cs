e1f06894765a6fc0302f6c29376a01e8ee823af8
Kianek <kallridge92@gmail.com>
2020-11-03 08:25:40 -0800

    Improve formatting

--- writely.tests/Journals/JournalServiceTest.cs
+++ writely.tests/Journals/JournalServiceTest.cs
@@ -23,21 +23,26 @@ namespace writely.tests.Journals
         [Fact]
         public async Task Add_UniqueTitle_JournalCreated()
         {
+            // Arrange
             const string userId = "UserId";
             const string title = "Super Nifty Journal";
 
             await PrepareDatabase();
             
             var service = new JournalService(_context);
+            
+            // Act
             var result = await service.Add(userId, title);
-    
             var addedJournal = await _context.Journals.FindAsync(result.Id);
+            
+            // Assert
             addedJournal.Title.Should().Be(title);
         }
 
         [Fact]
         public async void Add_ExistingTitle_ThrowsDuplicateJournalException()
         {
+            // Arrange
             const string userId = "UserId";
             const string title = "Super Samey Title";
 
@@ -46,6 +51,7 @@ namespace writely.tests.Journals
             _context.Journals.Add(new Journal {UserId = userId, Title = title});
             await _context.SaveChangesAsync();
             
+            // Act & assert
             var service = new JournalService(_context);
             service
                 .Invoking(js => js.Add(userId, title))
@@ -56,6 +62,7 @@ namespace writely.tests.Journals
         [Fact]
         public async void Update_ExistingJournal_TitleUpdated()
         {
+            // Arrange
             const string userId = "UserId";
             const string title = "Old Title";
             const string newTitle = "New Title";
@@ -65,6 +72,7 @@ namespace writely.tests.Journals
             _context.Journals.Add(new Journal {Id = 1L, UserId = userId, Title = title});
             await _context.SaveChangesAsync();
             
+            // Act
             var service = new JournalService(_context);
             var updatedJournal = new JournalDto
             {
@@ -75,17 +83,20 @@ namespace writely.tests.Journals
             var result = await service.Update(updatedJournal);
             var updJournal = await _context.Journals.FindAsync(updatedJournal.Id);
 
+            // Assert
             result.Title.Should().Be(updJournal.Title);
         }
         
         [Fact]
         public async void Update_JournalNotFound_NotUpdated()
         {
+            // Arrange
             await PrepareDatabase();
             
             var service = new JournalService(_context);
             var journal = new JournalDto {Id = 1L};
             
+            // Act & assert
             service.Invoking(s => s.Update(journal))
                 .Should()
                 .Throw<JournalNotFoundException>();
@@ -94,9 +105,11 @@ namespace writely.tests.Journals
         [Fact]
         public async Task Update_JournalFound_DuplicateTitle_ThrowsDuplicateJournalException()
         {
+            // Arrange
             await PrepareDatabase();
             PopulateJournals();
 
+            // Act & assert
             var journal = new JournalDto { Id = 1L, Title = "Journal 1", UserId = "UserId"};
             var service = new JournalService(_context);
             service
@@ -108,6 +121,7 @@ namespace writely.tests.Journals
         [Fact]
         public async void Delete_JournalFound_JournalRemoved()
         {
+            // Arrange
             var journal = new Journal { Id = 1L, Title = "Blah", UserId = "UserId"};
 
             await PrepareDatabase();
@@ -115,16 +129,19 @@ namespace writely.tests.Journals
             _context.Journals.Add(journal);
             await _context.SaveChangesAsync();
             
+            // Act
             var service = new JournalService(_context);
             await service.Delete(journal.Id);
-            
             var result = await _context.Journals.FindAsync(journal.Id);
+            
+            // Assert
             result.Should().BeNull();
         }
         
         [Fact]
         public async void GetAll_UserFound_NoJournalLimit_Success()
         {
+            // Arrange
             const string userId = "UserId";
 
             await PrepareDatabase();
@@ -136,15 +153,18 @@ namespace writely.tests.Journals
                 );
             await _context.SaveChangesAsync();
             
+            // Act
             var service = new JournalService(_context);
             var result = await service.GetAll(userId);
 
+            // Assert
             result.Count.Should().Be(3);
         }
         
         [Fact]
         public async void GetAll_UserFound_JournalLimit_ReturnsLimitedJournals()
         {
+            // Arrange
             const string userId = "UserId";
             
             await PrepareDatabase();
@@ -152,15 +172,18 @@ namespace writely.tests.Journals
             PopulateJournals(5);
             await _context.SaveChangesAsync();
             
+            // Act
             var service = new JournalService(_context);
             var result = await service.GetAll(userId, page: 3);
 
+            // Assert
             result.Should().HaveCount(3);
         }
 
         [Fact]
         public async void GetAll_UserFound_NoLimit_ReturnsAllJournals()
         {
+            // Arrange
             const string userId = "UserId";
 
             await PrepareDatabase();
@@ -168,15 +191,18 @@ namespace writely.tests.Journals
             PopulateJournals(4);
             await _context.SaveChangesAsync();
             
+            // Act
             var service = new JournalService(_context);
             var result = await service.GetAll(userId);
 
+            // Assert
             result.Should().HaveCount(4);
         }
         
         [Fact]
         public async void GetById_JournalFound_ReturnsJournalDto()
         {
+            // Arrange
             const long id = 1L;
             const string title = "Blah";
 
@@ -185,9 +211,11 @@ namespace writely.tests.Journals
             _context.Journals.Add(new Journal {Id = id, Title = title, UserId = "UserId"});
             await _context.SaveChangesAsync();
             
+            // Act
             var service = new JournalService(_context);
             var result = await service.GetById(id);
 
+            // Assert
             result.Should().NotBeNull();
             result.Title.Should().Be(title);
         }
@@ -195,7 +223,10 @@ namespace writely.tests.Journals
         [Fact]
         public async Task GetById_JournalNotFound_ThrowsJournalNotFoundException()
         {
+            // Arrange
             await PrepareDatabase();
+            
+            // Act & assert
             var service = new JournalService(_context);
             service
                 .Invoking(s => s.GetById(1L))
