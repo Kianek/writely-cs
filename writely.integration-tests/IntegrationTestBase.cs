71764439c4fb97f3c51f2a21973dca726aa0ec04
Kianek <kallridge92@gmail.com>
2020-11-12 07:29:18 -0800

    Refactor and rearrange code

--- writely.integration-tests/IntegrationTestBase.cs
+++ writely.integration-tests/IntegrationTestBase.cs
@@ -6,70 +6,96 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using writely.Data;
+using writely.Exceptions;
 using writely.Models;
 using writely.Models.Dto;
 using writely.Services;
 
 namespace writely.integration_tests
 {
-    public class IntegrationTestBase : IDisposable
+    public abstract class IntegrationTestBase : IDisposable
     {
+        private WebAppFactory<Startup> _factory;
+        private IServiceScope _scope;
+        private IServiceProvider _services;
         protected HttpClient _client;
-        protected IServiceScope _scope;
-        protected IServiceProvider _services;
+        protected AppUser _user;
 
         protected IntegrationTestBase(WebAppFactory<Startup> factory)
         {
-            _client = factory.CreateClient(
+            _factory = factory;
+        }
+
+        protected void InitializeTestHost()
+        {
+            _client = _factory.CreateClient(
                 new WebApplicationFactoryClientOptions
                 {
                     AllowAutoRedirect = false,
                     BaseAddress = new Uri("http://localhost:5000")
                 });
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
-            _scope = factory.Services.CreateScope();
+            _scope = _factory.Services.CreateScope();
             _services = _scope.ServiceProvider;
         }
 
-        protected ApplicationDbContext GetContext() => _services.GetRequiredService<ApplicationDbContext>();
+        protected virtual async Task ArrangeTest()
+        {
+                InitializeTestHost();
+                _user = await SetUpUser();
+        }
         
-        protected IUserService GetUserService() => _services.GetRequiredService<IUserService>();
+        protected T GetService<T>() => _services.GetRequiredService<T>();
+        
+        private void ResetDatabase()
+        {
+            var context = GetContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
 
-        public async Task<AppUser> GetUserByEmail(string email)
+        private ApplicationDbContext GetContext() => _services.GetRequiredService<ApplicationDbContext>();
+
+        private async Task<AppUser> GetUserByEmail(string email)
         {
             var context = GetContext();
 
             return await context.Users.FirstOrDefaultAsync(u => u.Email == email);
         }
 
-        public async Task SignInUser(UserRegistrationDto user)
+        private async Task SignInUser(UserRegistrationDto user)
         {
             var credentials = new UserLoginDto
                 {Email = user.Email, Password = user.Password, RememberMe = false};
             await _client.PostAsync("/api/auth/login", credentials.AsStringContent());
         }
 
-        public async Task<AppUser> SetUpUser()
+        private async Task<AppUser> SetUpUser()
         {
-            await ResetDatabase();
+            ResetDatabase();
             var newUser = Helpers.CreateRegistrationDto();
-            var service = GetUserService();
-            await service.Register(newUser);
-            await SignInUser(newUser);
+            var service = GetService<IUserService>();
+
+            try
+            {
+                await service.Register(newUser);
+            }
+            catch (DuplicateEmailException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                await SignInUser(newUser);
+            }
 
             return await GetUserByEmail(newUser.Email);
         }
-
-        public async Task ResetDatabase()
+        
+        public virtual void Dispose()
         {
-            var context = GetContext();
-            await context.Database.EnsureDeletedAsync();
-            await context.Database.EnsureCreatedAsync();
-        }
-
-        public void Dispose()
-        {
-            _scope.Dispose();
+            _client?.Dispose();
+            _user = null;
         }
     }
 }
